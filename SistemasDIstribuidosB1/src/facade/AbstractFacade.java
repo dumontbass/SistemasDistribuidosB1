c56d73a1b500adc4356bf3833f4f06b58869cf86
guilherme_ribeiro <guilhermedoceu@gmail.com>
2013-09-25 11:30:06 -0300

    primeiro commit

---
+++ SistemasDIstribuidosB1/src/facade/AbstractFacade.java
@@ -0,0 +1,86 @@
+package facade;
+
+
+
+
+import net.vz.mongodb.jackson.DBCursor;
+import net.vz.mongodb.jackson.JacksonDBCollection;
+import net.vz.mongodb.jackson.WriteResult;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guilherme
+ * Date: 8/21/13
+ * Time: 2:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class AbstractFacade<T> {
+
+    private JacksonDBCollection<T,String> jacksonDB;
+    private Class<T> entityClass;
+
+    public AbstractFacade(Class<T> entityClass, JacksonDBCollection<T, String> jacksonDB) {
+        this.entityClass = entityClass;
+        this.jacksonDB = jacksonDB;
+    }
+
+    public AbstractFacade(Class<T> entityClass) {
+
+        this.entityClass = entityClass;
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    protected AbstractFacade() {
+    }
+
+    protected abstract EntityManager getEntityManager();
+
+
+    public String create(T entity) {
+        WriteResult<? extends Object, String> result = jacksonDB.insert(entity);
+        return result.getSavedObject().toString();
+    }
+
+    public void edit(T entity) {
+        getEntityManager().merge(entity);
+    }
+
+    public void remove(T entity) {
+        getEntityManager().remove(getEntityManager().merge(entity));
+    }
+
+    public T find(Object id) {
+        return getEntityManager().find(entityClass, id);
+    }
+
+    public List<T> findAll() {
+
+        List<T> lista = new ArrayList<T>();
+        DBCursor<T> cursor =  jacksonDB.find();
+
+        while(cursor.hasNext()){
+            lista.add(cursor.next());
+        }
+
+
+        return lista;
+
+    }
+
+    public List<T> findRange(int[] range) {
+
+        return null;
+    }
+
+    public int count() {
+
+
+
+        return  findAll().size();
+    }
+
+}
---
+++ SistemasDIstribuidosB1/src/sistemasdistribuidosb1/SistemasDIstribuidosB1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemasdistribuidosb1;
+
+/**
+ *
+ * @author guilherme
+ */
+public class SistemasDIstribuidosB1 extends javax.swing.JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
